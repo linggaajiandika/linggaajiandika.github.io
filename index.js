966c4091ae9cd6190690bcb725c8f224900a1519
Lingga Aji Andika <linggaajiandika@noreply.github.com>
2020-05-12 19:58:00 +0700

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ var div = d3.select("body").append("div")
 
 var simulation = d3.forceSimulation()
     .force("link", d3.forceLink().id(function(d) { return d.id; }))
-    .force("charge", d3.forceManyBody().strength(2000))
+    .force("charge", d3.forceManyBody().strength(10))
     .force("center", d3.forceCenter(width / 2, height / 2));
 
 d3.json("graph.json", function(error, graph) {
